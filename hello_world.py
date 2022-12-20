ed1fde377d8e852e649aac156c67e35a57ce5335
npat15 <nicholas.r.patilsen@vanderbilt.edu>
2022-12-19 19:39:16 -0800

    extract scripts

--- hello_world.py
+++ hello_world.py
@@ -5,4 +5,8 @@ from bs4 import BeautifulSoup
 # specify mtn project user id and build url
 user_id = 200544978
 url = f"https://www.mountainproject.com/user/{user_id}"
-print(url)
+
+# get page html
+profile = requests.get(url)
+soup = BeautifulSoup(profile.content, "html.parser")
+print(soup.find_all("script"))
